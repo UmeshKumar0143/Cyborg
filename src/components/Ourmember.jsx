f3ac2311a72db43decca03cb8ba615bffb912751
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-10-14 07:31:53 +0530

    width

--- src/components/Ourmember.jsx
+++ src/components/Ourmember.jsx
@@ -46,9 +46,9 @@ const Ourmember = () => {
         <div>
          <h1 className={`xl:text-5xl text-center xl:text-left xl:ml-32 text-3xl  font-semibold`}>Our <span className='text-orange-400'>Members</span></h1>
          </div>
-         <div className='container xl:ml-32'>
+         <div className='container xl:ml-28'>
          <div className='flex gap-3 flex-wrap  mt-8 ml-10'>
-        {members.map((item,index)=> <div key={index} className="xl:w-[187px] w-[80px] h-[80px] border xl:h-[120px] flex justify-center items-end rounded-lg "
+        {members.map((item,index)=> <div key={index} className="xl:w-[147px] xl:h-[90px]  w-[80px]  h-[80px] border  flex justify-center items-end rounded-lg "
           style={{ backgroundImage: `url(${item.url})`,
             backgroundRepeat: "no-repeat",
             backgroundPosition: "center",
