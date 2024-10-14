0d7ca977c1334e45507d983a8fdd1b0fe4099031
UmeshKumar0143 <149981630+UmeshKumar0143@users.noreply.github.com>
2024-10-14 07:52:21 +0530

    container

--- src/components/Ourmember.jsx
+++ src/components/Ourmember.jsx
@@ -36,9 +36,6 @@ const members = [{url:member1,name: "Robert Brown"},
                  {url:member2,name: "Leslie Livingston"},    
                  {url:member3,name: "Joseph McFall"},    
                  {url:member4,name: "Helene Engels"}, 
-                 {url:member1,name: "Robert Brown"},
-                 {url:member2,name: "Leslie Livingston"},    
-                 {url:member3,name: "Joseph McFall"},       
 ]
 const Ourmember = () => {
   return (
@@ -46,7 +43,7 @@ const Ourmember = () => {
         <div>
          <h1 className={`xl:text-5xl text-center xl:text-left xl:ml-32 text-3xl  font-semibold`}>Our <span className='text-orange-400'>Members</span></h1>
          </div>
-         <div className='container xl:ml-20'>
+         <div className=' xl:ml-20'>
          <div className='flex gap-3 flex-wrap  mt-8 ml-10'>
         {members.map((item,index)=> <div key={index} className="xl:w-[147px] xl:h-[90px]  w-[80px]  h-[80px] border  flex justify-center items-end rounded-lg "
           style={{ backgroundImage: `url(${item.url})`,
